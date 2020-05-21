20ba47fe85fc4df52331206cf35ec36ec5d27bfe
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-21 21:23:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,16 +19,24 @@ find_item_by_name_in_collection("CANNED CORN", grocery_shelf)
 def item_in_bigarray? (item,array) 
   if find_item_by_name_in_collection(item,array) == nil 
     return false 
-  else find_index
+  else true
   end 
 end 
 
+def add_count_to_cart(cart) 
+  cart.each |hash| do 
+    hash[:count] = 1 
+  return cart 
+end 
+
 item_in_bigarray?("CANNED CORN" , grocery_shelf) 
+add_count_to_cart(grocery_shelf)  
 
 def consolidate_cart(cart)
   final_cart = []
   cart.each |itemhash| do 
     if item_in_bigarray?(itemhash[:item],final_cart)  
-      finalcart[:count] += 1 
+      ind = finalcart.index(itemhash) 
+      finalcart.ind[:count] += 1 
     else final 
 end
